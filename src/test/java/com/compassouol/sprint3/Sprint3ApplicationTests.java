a3e52764282e0973f64bc55e5564bb26a7465f0a
Leonardo <domafras@gmail.com>
2021-07-02 09:16:00 -0300

    Test fix

--- src/test/java/com/compassouol/sprint3/Sprint3ApplicationTests.java
+++ src/test/java/com/compassouol/sprint3/Sprint3ApplicationTests.java
@@ -1,13 +1,13 @@
 package com.compassouol.sprint3;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Sprint3ApplicationTests {
+public class Sprint3ApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
